7a0783f71b8262a2000cd16eba9ac2a9b8654f27
Anastasia <78533116+gelst13@users.noreply.github.com>
2022-03-03 12:47:29 +0400

    6/7
    seems the program works fine on my machine, but I get Wrong answer in test #2
    The program cannot sum more than two numbers.
    
    ---
    > 5 + 1 + 4
    expression: 5 + 1 + 4
    10

--- draft.py
+++ draft.py
@@ -1,51 +1,111 @@
 # $Smart Calculator
 """
-multiplication, division, addition, and subtraction
-+ the ability to calculate expressions containing parenthesis
-+ the ability to remember the previous result
-stage 1/7
-Write a program that reads two integer numbers from the same line
-and prints their sum in the standard output. Numbers can be positive, negative, or zero.
-stage 2/7
-program that reads two numbers in a loop and prints the sum in the standard output.
-If a user enters only a single number, the program should print the same number.
-If a user enters an empty line, the program should ignore it.
-When the command /exit is entered, the program must print "Bye!" and then stop.
-stage 3/7
-Add to the calculator the ability to read an unlimited sequence of numbers.
-Add a /help command to print some information about the program.
-stage 4/7
-the program must receive the addition + and subtraction - operators as an input
-It must support both unary and binary minus operators
-If the user has entered several same operators following each other,
-the program still should work (like Java or Python REPL).
-9 +++ 10 -- 8
-3 --- 5
-14        -  12
-two adjacent minus signs turn into a plus.
-/help command: maintain its relevance depending on the changes (in the next stages too).
-stage 5/7
-The program only knows numbers, a plus sign, a minus sign, and two commands.
-It cannot accept all other characters
+stage 6/7
+I decided to replace subtraction by adding negative value
 """
+
+
+class InvalidIdentifierError(Exception):
+    def __str__(self):
+        return "Invalid identifier!"
+
+
+class InvalidAssignmentError(Exception):
+    def __str__(self):
+        return "Invalid assignment"
 
 
 class Calculator:
     
-    def __init__(self, numbers):
+    def __init__(self):
         """The initializer of the class.
 
         Arguments:
-        numbers: tuple of integers
+        expression: string
         """
         # self.first = first
         # self.second = second
-        self.numbers = numbers
+        # self.numbers = numbers
+        self.variables = dict()
+        self.expression = None
     
-    def add(self):
-        """ Addition """
-        return sum(self.numbers)
     
+    @staticmethod
+    def store_variable_value(expr: str) -> dict:
+        """Process expressions like 'a = 2' or 'a = b'"""
+        var, value = expr.split('=')
+        var = var.strip()  # var is str here
+        value = value.strip()  # value is str here
+        try:
+            if not var.isalpha():  # wrong variable' name: must contain only Latin letters
+                raise InvalidIdentifierError
+            else:
+                return {var: int(value)}
+        except ValueError:
+            if not value.isalpha():  # bad reference: variable' name must be of Latin letters
+                raise InvalidAssignmentError
+            else:
+                return {var: value}
+    
+    def assign(self, expr: str):
+        new_entry = Calculator.store_variable_value(expr)
+        self.variables.update(new_entry)
+
+    def return_value(self, expr_: str):
+        """Expression like 'a' - variable name only.
+        return value of this variable from dict or return value of other variable
+        in case this variable's value is a reference to other variable."""
+        if not isinstance(self.variables[expr_], int):
+            other_key = self.variables[expr_]
+            print({expr_: self.variables[expr_]}, {other_key: self.variables[other_key]})
+            return self.variables[other_key]
+        else:
+            return self.variables[expr_]
+
+    def process_digits(self):
+        """Change self.expression: digit str -> int"""
+        for i, char in enumerate(self.expression):
+            try:
+                if char.isdigit():
+                    self.expression[i] = int(char)
+            except AttributeError:
+                continue
+
+    def process_variables(self):
+        """Change self.expression: digit str -> int"""
+        for i, char in enumerate(self.expression):
+            if char.isalpha():
+                self.expression[i] = Calculator.return_value(self, char)
+
+    def process_operators(self):
+        """Change self.expression: remove '+', stick '-' to next value."""
+        for i, char in enumerate(self.expression):
+            if char == '-':
+                negative = -self.expression[i + 1]
+                self.expression[i + 1] = negative
+                del self.expression[i]
+            elif char == '+':
+                del self.expression[i]
+
+    def add(self, expr: str) -> int:
+        """Save taken expr to self.expression and process(change) self.expression."""
+        self.expression = expr.split()
+        Calculator.process_variables(self)
+        # print(self.expression)
+        Calculator.process_digits(self)
+        # print(self.expression)
+        Calculator.process_operators(self)
+        # print(self.expression)
+        return sum(self.expression)
+    
+    
+    
+    
+    @staticmethod
+    def help():
+        print('The program knows two commands: /help, /exit.'
+              'It supports variables(storing them in a dict), addition, and subtraction.')
+
 
 def terminate():
     print('Bye!')
@@ -53,30 +113,58 @@ def terminate():
 
 
 def main():
+    # collection = ('a', 'n = 33', 'm=4', 'a  =   5', 'b = n', 'b', 'v=   7', 'n =9', 'count = 10',
+    #               'a2 = 1', 'a = 2', 'a = 3', 'a', 'a2a', 'n22', 'n = a2a',
+    #               '2  + 3  -  a',)
+    # collection1 = ('a  =  3', 'b= 4', 'c =5', 'a + b - c', 'b - c + 4 - a', 'a = 800',
+    #                'a + b + c', 'BIG = 9000', 'BIG', 'big', '/exit',)
+    # new = Calculator()
     while True:
+        new = Calculator()
         user_input = input()
-        if user_input in (' ', ''):
+    # for expr_ in collection1:
+        # user_input = expr_
+        if user_input in (' ', ''):  # empty line is ignored
             main()
+        elif user_input == '/exit':
+            terminate()
+        elif user_input == '/help':
+            Calculator.help()
+            continue
         try:
-            print(eval(user_input))
+            # user_input = expr_
+            print(f'expression: {user_input}')
+            if '=' in user_input:
+                Calculator.assign(new, user_input)
+                # print(f'new.variables = {new.variables}')
+            elif '+' in user_input or '-' in user_input:
+                print(Calculator.add(new, user_input))
+            # if it's variable name
+            else:
+                if not user_input.isalpha():  # bad variable name
+                    raise InvalidIdentifierError
+                else:
+                    print(Calculator.return_value(new, user_input))
+        except InvalidIdentifierError as err:
+            print(err)
+            continue
+        except InvalidAssignmentError as err:
+            print(err)
+            continue
+        except KeyError:
+            print('Unknown variable')
+            continue
         except NameError:
             print('Invalid expression')
             main()
         except SyntaxError:
-            if user_input == '/exit':
-                terminate()
-            elif user_input == '/help':
-                print('The program only knows numbers, a plus sign, a minus sign, and two commands:'
-                      '/help, /exit. It cannot accept any other characters.')
-                main()
-            elif user_input[0] == '/':
+            if user_input[0] == '/':
                 print('Unknown command')
-                main()
-            elif user_input in (' ', ''):
                 main()
             else:
                 print('Invalid expression')
                 main()
+    print(new.variables)
 
 
 if __name__ == '__main__':
