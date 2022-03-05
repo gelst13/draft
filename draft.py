2a11414672b1b05f15595c617686d5f466169333
Anastasia <78533116+gelst13@users.noreply.github.com>
2022-03-05 14:39:24 +0400

    implemented re for parsing

--- draft.py
+++ draft.py
@@ -2,14 +2,16 @@
 """
 stage 6/7
 I decided to replace subtraction by adding negative value
+implemented regex fro parsing:
+1) expression of type 'assignment ='
+2) expressions with operators + -
 """
 import logging
+import re
 
 logging.basicConfig(filename='my_calc.log', level=logging.DEBUG, filemode='a',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-logging.disable(10)
+                    format='%(levelname)s - %(message)s')
+# logging.disable(10)
 
 # logging.debug('DEBUG message')
 # logging.info('INFO message')
@@ -20,7 +22,7 @@ logging.disable(10)
 
 class InvalidIdentifierError(Exception):
     def __str__(self):
-        return "Invalid identifier!"
+        return "Invalid identifier"
 
 
 class InvalidAssignmentError(Exception):
@@ -34,6 +36,7 @@ class Calculator:
         """The initializer of the class.
 
         Arguments:
+        variables: dict
         expression: string
         """
         # self.first = first
@@ -42,48 +45,65 @@ class Calculator:
         self.variables = dict()
         self.expression = list()
     
+    
     def process_assignment(self, expr: str) -> dict:
         """Process expressions like 'a = 2' or 'a = b'"""
-        var, value = expr.split('=')
+        logging.info('executing process_assignment(self, expr: str)...')
+        logging.info("parse expression of type 'assignment ='")
+        parse = re.match(r'^\s*(\w+)\s*=\s*(\w+)\s*$', expr)
+        var, value = parse[1], parse[2]
+        # var, value = expr.split('=')
         var = var.strip()  # var is str here
-        # logging.debug(f'len {var} = {len(var)}')
+        logging.debug(f'var = {var}')
         value = value.strip()  # value is str here
-        # logging.debug(f'len {value} = {len(value)}')
+        logging.debug(f'value= {value} ')
         try:
             if not var.isalpha():  # check left side if wrong variable' name
+                logging.info('during assignment - check_identifier (left part)')
+                logging.debug(f'not var.isalpha()? {not var.isalpha()}')
                 raise InvalidIdentifierError
             elif (not value.isdigit()) and (value not in list(self.variables.keys())):
+                logging.info('during assignment - check_right_part')
+                logging.debug('right_part is not digit and is not stored variable')
                 raise InvalidAssignmentError
             else:
+                logging.info('during assignment - check_right_part')
                 return {var: int(value)}
         except ValueError:
             if not value.isalpha():  # # check right side if wrong variable' name
+                logging.info('raise InvalidAssignmentError')
                 raise InvalidAssignmentError
             else:
+                logging.info('success! return {var: other_var}')
                 return {var: value}
     
     def assign(self, expr: str):
-        new_entry = Calculator.process_assignment(self, expr)
+        logging.info('executing assign(self, expr: str)...')
+        new_entry = self.process_assignment(expr)
         logging.debug(f'new_entry = {new_entry}')
         self.variables.update(new_entry)
-        logging.debug(f'self.variables = {self.variables}')
+        logging.debug(f'success! self.variables = {self.variables}')
     
     def return_value(self, expr_: str):
         """Expression like 'a' - variable name only.
         return value of this variable from dict or return value of other variable
         in case this variable's value is a reference to other variable."""
-        logging.debug('start executing return_value(self, expr_: str)')
+        logging.info('executing return_value(self, expr_: str)...')
         if not isinstance(self.variables[expr_], int):
-            logging.debug(not isinstance(self.variables[expr_], int))
+            logging.info('return_value when {var: other_var}')
             other_key = self.variables[expr_]
             logging.debug({expr_: self.variables[expr_]}, {other_key: self.variables[other_key]})
+            logging.debug(f'=> {expr_} = {self.variables[other_key]}')
             return self.variables[other_key]
         else:
-            logging.debug(f"self.variables[{expr_}] {self.variables[expr_]}")
+            logging.info('return_value when {var: int}')
+            logging.debug(f"self.variables[{expr_}]= {self.variables[expr_]}")
             return self.variables[expr_]
     
     def process_digits(self):
         """Change self.expression: digit str -> int"""
+        logging.info('executing process_digits(self)...')
+        logging.debug(self.expression)
         for i, char in enumerate(self.expression):
             try:
                 if char.isdigit():
@@ -93,13 +113,16 @@ class Calculator:
     
     def process_variables(self):
         """Change self.expression: digit str -> int"""
+        logging.info('executing process_variables(self)...')
+        logging.debug(self.expression)
         for i, char in enumerate(self.expression):
             if char.isalpha():
-                self.expression[i] = Calculator.return_value(self, char)
+                self.expression[i] = self.return_value(char)
     
     def process_operators(self):
         """Change self.expression: remove '+', stick '-' to next value."""
-        
+        logging.info('executing process_operators(self)...')
+        logging.debug(self.expression)
         for i, char in enumerate(self.expression):
             if char == '-':
                 negative = -self.expression[i + 1]
@@ -107,21 +130,31 @@ class Calculator:
                 del self.expression[i]
             elif char == '+' or char == '++':
                 del self.expression[i]
-    
+
     def add(self, expr: str) -> int:
         """Cast taken expr to list, save it in self.expression
         process(change) self.expression."""
+        logging.info('executing add(self, expr: str)...')
+        logging.debug(f'expr: {expr}')
+        logging.info('process adjacent operators like -- or ---')
+        expr = expr.replace(' ', '')
         expr = expr.replace('---', '-')
+        expr = expr.replace('+++', '+')
         expr = expr.replace('--', '+')
-        self.expression = expr.split()
-        Calculator.process_variables(self)
+        expr = expr.replace('++', '+')
+        logging.debug(f'{expr}')
+        # parse expressions with operators
+        self.expression = re.findall(r'([\-+]|\w+|[^\s\w\-+]+)', expr)
+        # self.expression = expr.split()
+        logging.debug(f'self.expression = {self.expression}')
+        self.process_variables()
         logging.debug(self.expression)
-        Calculator.process_digits(self)
+        self.process_digits()
         logging.debug(self.expression)
-        Calculator.process_operators(self)
+        self.process_operators()
         logging.debug(self.expression)
         return sum(self.expression)
-    
+   
     @staticmethod
     def help():
         print('The program knows two commands: /help, /exit.'
@@ -141,47 +174,50 @@ def main():
     #                'a + b + c', 'BIG = 9000', 'BIG', 'big', '/exit',)
     # collection3 = ('a = 9', 'b=2', 'c = 1', 'a + b', 'b - a', 'b + c', 'b - c',
     #                'a -- b - c + 3 --- a ++ 1')
+    collection4 = ('n = 2', 'n = 3', 'n', 'a  =   5', 'b = a', 'b', 'a = 8', 'b = c', 'a1 = 8',
+                   'n1 = a2a', 'n = a2a', 'a = 7 = 8', 'a2a', 'e', 'a=1', 'a', '1 + 2 - 3',
+                   'b - c + 4 - a', '1 ++ 2 -- 3 --- 4')
     new = Calculator()
+    logging.info('new = Calculator()')
     while True:
         user_input = input()
-        logging.debug(f'expression: {user_input}')
-        # for expr_ in collection3:
-        #     user_input = expr_
-        #     logging.debug(f'expression: {user_input}')
+        logging.debug(f'user_input: {user_input}')
+    # for expr_ in collection4:
+    #     user_input = expr_
+    #     logging.debug(f'user_input: {user_input}')
         if user_input in (' ', ''):  # empty line is ignored
-            logging.info("if user_input in (' ', '')")
+            logging.info('empty line is ignored')
             main()
         elif user_input == '/exit':
-            logging.info("elif user_input == '/exit'")
+            logging.info("user_input == '/exit'")
             terminate()
         elif user_input == '/help':
-            logging.info("elif user_input == '/help'")
+            logging.info("user_input == '/help'")
             Calculator.help()
             continue
         try:
-            # user_input = expr_
-            # logging.debug(f'expression: {user_input}')
-            logging.info('TRY:')
+            logging.info('try:')
             if user_input.count('=') > 1:
+                logging.info("assignment: check for multiple '='")
                 raise InvalidAssignmentError
             elif '=' in user_input:
-                logging.info("if '=' in user_input ")
+                logging.info("detected assignment =")
                 Calculator.assign(new, user_input)
-            
             elif '+' in user_input or '-' in user_input:
-                logging.info("elif '+' in user_input or '-' in user_input")
+                logging.info("calculations of different expressions")
                 print(Calculator.add(new, user_input))
             # if it's variable name
             else:
                 logging.info('ELSE')
                 if user_input[0] == '/':
-                    logging.debug("user_input[0] == '/'\nmessage: Unknown command")
+                    logging.debug("user_input[0] == '/..'\nmessage: Unknown command")
                     print('Unknown command')
                     main()
-                elif not user_input.isalpha():  # bad variable name
+                elif not user_input.isalpha():  # bad variable name -> True
+                    logging.info('call variable: check_identifier')
                     raise InvalidIdentifierError
                 else:
-                    logging.info('call Calculator.return_value(new, user_input)')
+                    logging.info('call variable: check_if_variable_stored')
                     print(Calculator.return_value(new, user_input))
         except InvalidIdentifierError as err:
             print(err)
@@ -190,6 +226,7 @@ def main():
             print(err)
             continue
         except KeyError as err:
+            logging.debug('check_if_variable_stored: False')
             logging.warning(err)
             print('Unknown variable')
             continue
@@ -199,10 +236,6 @@ def main():
             main()
         except SyntaxError as err:
             logging.warning(err)
-            # if user_input[0] == '/':
-            #     print('Unknown command')
-            #     main()
-            # else:
             print('Invalid expression')
             main()
 
