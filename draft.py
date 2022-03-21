56c478a70d7365b3f21d93faa2e5e31703e3b1c3
Anastasia <78533116+gelst13@users.noreply.github.com>
2022-03-21 16:53:34 +0400

    3/5

--- draft.py
+++ draft.py
@@ -1,128 +1,179 @@
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding: utf-8
+# $Static Code Analyzer
 import logging
-from pprint import pprint
+import os
 import re
-
+import sys
 
 logging.basicConfig(filename='bo.log', level=logging.DEBUG, filemode='a',
                     format='%(levelname)s - %(message)s')
-logging.disable(10)
+
+# logging.disable(10)
+
+# os.chdir(r'C:\Users\Тоша\PycharmProjects\Static Code Analyzer\Static Code Analyzer\task')
+# os.chdir(r'C:\Users\Тоша\PycharmProjects\FirstProject')
+
+target = sys.argv[1]
+logging.debug(target)
 
 
-def check_length(text):
-    if len(text) > 79:
-        return 'T00 long'
-
-
-def check_indentation(expr: str):
-    """Check if Indentation is a multiple of 4."""
-    if expr.startswith(' '):
-        parse = re.match(r'\ +', expr)
-        return 'Ok' if len(parse.group()) % 4 == 0 else 'Not Ok'
-
-
-def check_semicolon(expr: str):
-    # logging.info('check_semicolon...')
-    if expr.startswith('#'):
-        return False
-    elif expr.find(';'):
-        # logging.debug(f'expr: {expr}')
-        # case with inline comment expr = 'pass; # comment;'
-        parse = re.match(r'([\S\D]+)(#[\S\D]+)', expr)  # match='pass; # comment;
-        # logging.debug(f'parse= {parse}')
-        if parse:
-            statement = parse.group(1).strip()
-        # case without inline comment
+class StaticCodeAnalyzer:
+    
+    def __init__(self, _target: str):
+        self.target = _target
+        self.mistakes = dict()  # {'line' : [errors]}
+        self.messages = {'S001': 'T00 long',
+                         'S002': 'Indentation is not a multiple of four',
+                         'S003': 'Unnecessary semicolon after a statement',
+                         'S004': 'Less than two spaces before inline comments',
+                         'S005': 'TODO found',
+                         'S006': 'More than two blank lines preceding a code line',
+                         }
+    
+    def get_abs_names(self) -> list:
+        """Walking a directory tree and printing the names of the directories and files"""
+        logging.debug('get_abs_names...')
+        abs_names = []
+        for dirpath, dirnames, files_ in os.walk(self.target, topdown=True):
+            logging.debug(dirpath)
+            for file_name in files_:
+                if file_name.endswith('.py'):
+                    logging.debug((dirpath, '\\' + file_name))
+                    abs_names.append(os.path.join(dirpath, file_name))
+        return abs_names
+    
+    @staticmethod
+    def check_length(text):
+        if len(text) > 79:
+            return 'T00 long'
+    
+    @staticmethod
+    def check_indentation(expr: str):
+        """Check if Indentation is a multiple of 4."""
+        if expr.startswith(' '):
+            parse = re.match(r'\ +', expr)
+            return 'Ok' if len(parse.group()) % 4 == 0 else 'Not Ok'
+    
+    @staticmethod
+    def check_semicolon(expr: str):
+        # logging.info('check_semicolon...')
+        if expr.startswith('#'):
+            return False
+        elif expr.find(';'):
+            # logging.debug(f'expr: {expr}')
+            # case with inline comment expr = 'pass; # comment;'
+            parse = re.match(r'([\S\D]+)(#[\S\D]+)', expr)  # match='pass; # comment;
+            # logging.debug(f'parse= {parse}')
+            if parse:
+                statement = parse.group(1).strip()
+            # case without inline comment
+            else:
+                statement = expr.strip()
+            # logging.debug(f'statement= {statement}')
+            # logging.debug(statement.endswith(';'))
+            return statement.endswith(';')
+    
+    @staticmethod
+    def check_inline_comment(expr: str):
+        """Return True if there are less than 2 spaces between statement and # """
+        # logging.debug('check_inline_comment...')
+        # logging.debug(expr)
+        if expr.find('#') > 0:
+            # logging.debug(expr.find('#'))
+            comment_start = expr.index('#')
+            # logging.debug(expr.index('#'))
+            return expr[comment_start - 2:comment_start:1] != '  '
         else:
-            statement = expr.strip()
-        # logging.debug(f'statement= {statement}')
-        # logging.debug(statement.endswith(';'))
-        return statement.endswith(';')
-
-
-def check_inline_comment(expr: str):
-    """Return True if there are less than 2 spaces between statement and # """
-    # logging.debug('check_inline_comment...')
-    # logging.debug(expr)
-    if expr.find('#') > 0:
-        # logging.debug(expr.find('#'))
-        comment_start = expr.index('#')
-        # logging.debug(expr.index('#'))
-        return expr[comment_start - 2:comment_start:1] != '  '
-    else:
-        return None
-
-
-def check_todo(expr: str):
-    """Check if Less than 2 spaces before inline comments."""
-    logging.info('check_todo')
-    if expr.startswith('#'):
-        logging.debug(f'expr: {expr}')
-        parse = re.match(r'#.?[tT][Oo][Dd][Oo]', expr)
-        logging.debug(f'parse: {parse}')
-        if parse:
-            return True
-    elif expr.find('#'):
-        logging.debug(f'expr: {expr}')
-        parse = re.findall(r'# ?[tT][Oo][Dd][Oo]', expr)
-        logging.debug(f'parse: {parse}')
-        if parse:
-            return True
-
-
-messages = {'S001': 'T00 long',
-            'S002': 'Indentation is not a multiple of four',
-            'S003': 'Unnecessary semicolon after a statement',
-            'S004': 'Less than two spaces before inline comments',
-            'S005': 'TODO found',
-            'S006': 'More than two blank lines preceding a code line',
-            }
-mistakes = dict()  # {'line' : [errors]}
-
-
-# filename = r'C:\Users\Тоша\PycharmProjects\try.py'
-# filename = 'r' + input()
-filename = input()
-# filename = 'bad.py'
-# filename = 'test_3.py'
-empty_line_count = 0
-with open(filename, 'r', encoding='utf-8') as file:
-    for number, line in enumerate(file):
-        mistakes[number + 1] = []
-        if check_length(line):
-            mistakes[number + 1].append('S001')
-        if check_indentation(line) == 'Not Ok':
-            mistakes[number + 1].append('S002')
-        if check_semicolon(line):
-            logging.debug(number + 1)
-            mistakes[number + 1].append('S003')
-        if check_inline_comment(line):
-            mistakes[number + 1].append('S004')
-        if check_todo(line):
-            mistakes[number + 1].append('S005')
-        if line == '\n':
-            empty_line_count += 1
+            return None
+    
+    @staticmethod
+    def check_todo(expr: str):
+        """Check if Less than 2 spaces before inline comments."""
+        # logging.info('check_todo')
+        if expr.startswith('#'):
+            # logging.debug(f'expr: {expr}')
+            parse = re.match(r'#.?[tT][Oo][Dd][Oo]', expr)
+            # logging.debug(f'parse: {parse}')
+            if parse:
+                return True
+        elif expr.find('#'):
+            # logging.debug(f'expr: {expr}')
+            parse = re.findall(r'# ?[tT][Oo][Dd][Oo]', expr)
+            # logging.debug(f'parse: {parse}')
+            if parse:
+                return True
+    
+    def check_file(self, filename):
+        logging.info('check_file...')
+        logging.debug(f'filename: {filename}')
+        with open(filename, 'r', encoding='utf-8') as file:
+            empty_line_count = 0
+            for number, line in enumerate(file):
+                self.mistakes[number + 1] = []
+                if self.check_length(line):
+                    self.mistakes[number + 1].append('S001')
+                if self.check_indentation(line) == 'Not Ok':
+                    self.mistakes[number + 1].append('S002')
+                if self.check_semicolon(line):
+                    # logging.debug(number + 1)
+                    self.mistakes[number + 1].append('S003')
+                if self.check_inline_comment(line):
+                    self.mistakes[number + 1].append('S004')
+                if self.check_todo(line):
+                    self.mistakes[number + 1].append('S005')
+                if line == '\n':
+                    empty_line_count += 1
+                else:
+                    if empty_line_count > 2:
+                        self.mistakes[number + 1].append('S006')
+                        empty_line_count = 0
+    
+    def process_result(self):
+        new_m = dict()
+        for key, value in self.mistakes.items():
+            if value:
+                new_m[key] = sorted(list(set(value)))
+        return new_m
+    
+    def print_sorted_results(self, file_):
+        """Line X: Code Message"""
+        for lineno in sorted(self.mistakes.keys()):
+            for error in self.mistakes[lineno]:
+                print(f'{file_}: Line {lineno}: {error} {self.messages[error]}')
+    
+    def start(self) -> None:
+        # PREPARATION
+        if self.target.endswith('.py'):
+            logging.info('case 1')
+            files = [self.target, ]
         else:
-            if empty_line_count > 2:
-                mistakes[number + 1].append('S006')
-                empty_line_count = 0
+            logging.info('case 2')  # directory
+            files = self.get_abs_names()
+        logging.debug(f'files: {files}')
+        # ANALYZING
+        logging.info('ANALYZING')
+        for file in sorted(files):
+            # logging.debug(file)
+            self.check_file(file)
+            self.process_result()
+            self.print_sorted_results(file)
+            self.mistakes = dict()
 
 
-def process_result(data: dict):
-    new_m = dict()
-    for key, value in mistakes.items():
-        if value:
-            new_m[key] = sorted(list(set(value)))
-    return new_m
+# TESTING input
+# input_path = 'test\test_1.py'
+# input_path = r'C:\Users\Тоша\PycharmProjects\FirstProject\test'
+# input_path = '\test'
+# input_path = '\task'
+logging.info('START')
+# logging.debug(f'input: {input_path}')
 
+# target = input_path.replace('\t', '\\t')
+# target = target.replace('\a', '\\a')
+# logging.debug(f'target: {target}')
 
-def print_sorted_results(data: dict):
-    """Line X: Code Message"""
-    for lineno, errors in data.items():
-        for error in errors:
-            print(f'Line {lineno}: {error} {messages[error]}')
+new = StaticCodeAnalyzer(target)
 
-
-mistakes = process_result(mistakes)
-print_sorted_results(mistakes)
+if __name__ == '__main__':
+    new.start()
