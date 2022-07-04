0fd89eaf7f7826d9a2b74d996d0f65a224f1db7f
Anastasia <78533116+gelst13@users.noreply.github.com>
2022-07-04 12:03:04 +0400

    Update draft.py

--- draft.py
+++ draft.py
@@ -3,20 +3,20 @@
 DESCRIPTION:
 command-line script that allows you to:
 - keep info about your contacts' time zones
-- display: 1) "what time is it now at his place?"
-               [my current local time] and [contact's current time]
-           2) "what time should we plan for a call? what time will be at his place when it's .. o'clock here?"
-               [my local time] and [contact's time]
+- display: "what time is it now at smbd's place?"
+            [my current local time] and [contact's current time]
 - easily convert time (my local time into chosen time zone and vice versa)
+"what time should we plan for a call? what time will be at his place when it's .. o'clock here?"
+            [my local time] and [contact's time]
 
 settings: my local time zone is UTC +3
 -----
 SQL
-contact = (PK) contact_name / platform / comment / location / zone_name / difference_to_utc
+contact = (PK) contact_name / platform / comment / location / zone_name / utc_offset
 keep info in sql db
 filter contact's list by:
 - time zone
-- difference_to_utc
+- utc_offset
 - communicating platform
 ----
 menu:
@@ -34,16 +34,9 @@ menu:
 * user can type contact_names/platform in any registry, but the program capitalize them
 ----
 tasks:
-def convert_time(self):
-    a. my local time into chosen time zone
-    b. vice versa
+- unique names: don't allow add existing names, warn about that and offer: change contact, add, go back
 
 
-
-
-
-- retrieve your current time zone from your device settings
-- convert float time like 2.5 hours
 - make it work from command-line
 - clean it, check code style
 - try refactoring technics
@@ -63,13 +56,6 @@ logging.basicConfig(filename='bo.log', level=logging.DEBUG, filemode='a',
 
 
 class InfoBase:
-    # def __init__(self, contact_name, platform, comment, location, zone_name, difference_to_utc):
-    #     self.contact_name = contact_name
-    #     self.platform = platform
-    #     self.comment = comment
-    #     self.location = location
-    #     self.zone_name = zone_name
-    #     self.difference_to_utc = difference_to_utc
     def __init__(self):
         self.n = 0
 
@@ -83,17 +69,19 @@ class InfoBase:
                                                                   comment VARCHAR(255), 
                                                                   location VARCHAR(255),
                                                                   zone_name VARCHAR(255), 
-                                                                  difference_to_utc FLOAT)""")
+                                                                  utc_offset FLOAT)""")
             conn.commit()
 
     @staticmethod
-    def transfer_to_sql(contact_name, platform, comment, location, zone_name, difference_to_utc):
+    def transfer_to_sql(contact_name, platform, comment, location, zone_name, utc_offset):
         with sqlite3.connect('TimeZoneReminder.db') as conn:
             cursor = conn.cursor()
             cursor.execute(
-                "INSERT INTO contact(contact_name, platform, comment, location, zone_name, difference_to_utc)"
-                " VALUES (?, ?, ?, ?, ?, ?)", (contact_name, platform, comment, location, zone_name, difference_to_utc))
+                "INSERT INTO contact(contact_name, platform, comment, location, zone_name, utc_offset)"
+                " VALUES (?, ?, ?, ?, ?, ?)", (contact_name, platform, comment, location, zone_name, utc_offset))
             conn.commit()
+
+
 
     @staticmethod
     def select_column(column_name):
@@ -123,7 +111,7 @@ class InfoBase:
         # , as they are vulnerable to SQL injection attacks
         with sqlite3.connect('TimeZoneReminder.db') as conn:
             cursor = conn.cursor()
-            if column == 'difference_to_utc':
+            if column == 'utc_offset':
                 cursor.execute('SELECT * FROM contact WHERE %s = "%f"' % (column, key_word))
             else:
                 cursor.execute('SELECT * FROM contact WHERE %s = "%s"' % (column, key_word))
@@ -145,24 +133,25 @@ class InfoBase:
             if conn:
                 conn.close()
 
-
     @staticmethod
-    def print_contact_table():
+    def select_all():
         with sqlite3.connect('TimeZoneReminder.db') as conn:
             cursor = conn.cursor()
             cursor.execute('select * from contact')
             data_ = cursor.fetchall()
             conn.commit()
-        print('the table contact / TimeZoneReminder.db: ')
-        print('| contact_name | platform | comment | location | zone_name | difference_to_utc |')
-        print(*data_, sep='\n')
+        return data_
 
-    # def run(self):
-        # self.create_table()
-        # new_contact = ('ko', 'home', 'cat', 'Vlg', None, 0,)
-        # transfer_to_sql(contact_name, platform, comment, location, zone_name, difference_to_utc)
-        # self.transfer_to_sql()
-        # self.print_contact_table()
+    @staticmethod
+    def print_contact_table():
+        print('table: contact / TimeZoneReminder.db: ')
+        print('| contact_name  | platform | comment                          | location | zone_name | utc_offset |')
+        content = InfoBase.select_all()
+        for entry in content:
+            # print(*entry, sep=' | ')
+            print('{0:15} | {1:10} | {2:50} | {3:6}  | {4:9}  | {5:9} '.format(entry[0],
+                  entry[1], entry[2], entry[3], str(entry[4]), str(entry[5])))
+        print(f'Number of entries: {len(content)}')
 
 
 class TimeKeeper:
@@ -171,7 +160,7 @@ class TimeKeeper:
         self.command = ''
         self.call = 0
 
-    # {'zone_short_name': (difference_to_utc, zone_ful_name, 1 - if seasonal
+    # {'zone_short_name': (utc_offset, zone_ful_name, 1 - if seasonal
     # time_zones keeps standard(winter) time for seasonal zones
     # summer daylight time = standard(winter) zone time - 1)}
     time_zones = {'UTC': (0, ''),
@@ -208,23 +197,23 @@ class TimeKeeper:
         return str(time2)[:5]
 
     @staticmethod
-    def show_current_time():
+    def show_current_time(tz_data):
         """convert current local time into another time zone
         Return str"""
-
-        tz_data = input('Enter the name of time zone or offset (hours of time difference) to UTC/GMT:> ')
+        print(tz_data)
         try:
             offset = datetime.timedelta(hours=float(tz_data))
             tz_ = datetime.timezone(offset)
-            # return datetime.datetime.now(tz_).strftime('%d-%m-%Y %H:%M')
+            return datetime.datetime.now(tz_).strftime('%d-%m-%Y %H:%M')
         except ValueError:
-            try:
-                tz_ = pytz.timezone(TimeKeeper.tz_olson[tz_data.upper()])
-                # return
-            except KeyError as e:
-                print(f'there are no {tz_data.upper()} time zone in my database. Try again with offset')
-                return False
-        print(f"current time in {tz_data} time zone: {datetime.datetime.now(tz_).strftime('%d-%m-%Y %H:%M')}")
+            # try:
+            tz_ = pytz.timezone(TimeKeeper.tz_olson[tz_data.upper()])
+            return datetime.datetime.now(tz_).strftime('%d-%m-%Y %H:%M')
+            # except KeyError as e:
+            #     print(f'there are no {tz_data.upper()} time zone in my database. Try again with offset')
+            #     return False
+        # print(f"current time in {tz_data} time zone: {datetime.datetime.now(tz_).strftime('%d-%m-%Y %H:%M')}")
+
 
     @staticmethod
     def date_constructor(zone_info, date: list, time0: list):
@@ -304,7 +293,10 @@ bbb - go back
                 self.time_operation()
 
             if operation == '1':
-                self.show_current_time()
+                tz_data = input('Enter the name of time zone or offset (hours of time difference) to UTC/GMT:> ')
+                time_now = self.show_current_time(tz_data)
+                if time_now:
+                    print(f"current time in {tz_data} time zone: {time_now}")
             elif operation == '2':
                 from_local = input('convert local time? y/n ')
                 if from_local.lower() == 'y':
@@ -325,27 +317,42 @@ bbb - go back
 
     def see_info(self):
         self.call += 0
-        choice = input('\nshow list of contacts? Y/N:> ')
-        if choice.lower() == 'y':
-            print(sorted(x.capitalize() for x in InfoBase.select_column('contact_name')))
-        elif choice.lower() == 'n':
-            data_for_search = input('Enter data for search(contact name/platform/time zone/time difference):> ')
-            if data_for_search.capitalize() in InfoBase.select_column('platform'):
-                print(f'Contacts from {data_for_search.capitalize()}')
-                print(InfoBase.select_row('platform', data_for_search.capitalize()))
-            elif data_for_search in TimeKeeper.time_zones.keys():
-                print(f'Contacts from {data_for_search}')
-                print(InfoBase.select_row('zone_name', data_for_search.upper()))
-            elif re.match('[-+]?[0-9.]', data_for_search):
-                print(f'Contacts from UTC {data_for_search}:')
-                print(InfoBase.select_row('difference_to_utc', float(data_for_search)))
+        print('\nList of contacts: ')
+        print(sorted(x.capitalize() for x in InfoBase.select_column('contact_name')))
+        if not self.check_if_db_empty():
+            self.start()
+        data_for_search = input('Enter data for search(contact name/platform/time zone/time difference):> ')
+        if data_for_search.capitalize() in InfoBase.select_column('platform'):
+            result = InfoBase.select_row('platform', data_for_search.capitalize())
+            print(f'Contacts from {data_for_search.capitalize()}:')
+            print(result if result else 'no such entries')
+        elif data_for_search in TimeKeeper.time_zones.keys():
+            result = InfoBase.select_row('zone_name', data_for_search.upper())
+            print(f'Contacts from {data_for_search}')
+            print(result if result else 'no such entries')
+        elif re.match('[-+]?[0-9.]', data_for_search):
+            result = InfoBase.select_row('utc_offset', float(data_for_search))
+            print(f'Contacts from UTC {data_for_search}:')
+            print(result if result else 'no such entries')
+        else:
+            print('Contact info:')
+            # [('Bo', 'Discord', 'Bocanada Hyperskill star', 'Seul', 'JST', 9.0)]
+            data = InfoBase.select_row('contact_name', data_for_search.capitalize())
+            print(data == list())
+            if data == list():
+                print('no such entries')
+                self.start()
             else:
-                print('Contact info:')
-                print(InfoBase.select_row('contact_name', data_for_search.capitalize()))
-                print(f'time for {data_for_search.capitalize()} now: ...')
+                print(data[0])
+                if data[4]:
+                    contact_time_now = self.show_current_time(data[0][4])
+                else:
+                    contact_time_now = self.show_current_time(data[0][5])
+                if contact_time_now:
+                    print(f'time for {data_for_search.capitalize()} now: {contact_time_now}')
 
     def add_contact(self):
-        # contact = (PK) contact_name / platform / comment / location / zone_name / difference_to_utc
+        # contact = (PK) contact_name / platform / comment / location / zone_name / utc_offset
         while True:
             contact_name = input('\nEnter contact name/nick:> ').capitalize()
             # ! contact names must be unique, check_contact_name uniqueness:
@@ -360,22 +367,22 @@ bbb - go back
                 time_zone = input('Enter the name of time zone or hours of time difference to UTC/GMT:> ')
                 if time_zone.isalpha():
                     zone_name = time_zone.upper()
-                    difference_to_utc = None
+                    utc_offset = None
                     break
                 elif re.match('[-+]?[0-9.]', time_zone):
                     zone_name = None
-                    difference_to_utc = float(time_zone)
+                    utc_offset = float(time_zone)
                     break
                 else:
                     print('Incorrect format. Try again!')
-        self.new_contact = (contact_name, platform, comment, location, zone_name, difference_to_utc)
+        self.new_contact = (contact_name, platform, comment, location, zone_name, utc_offset)
         print(self.new_contact)
         # sql_operation = InfoBase(*self.new_contact)
         # sql_operation.run()
         InfoBase.transfer_to_sql(*self.new_contact)
 
     def change_contact(self):
-        # [ contact_name / platform / comment / location / zone_name / difference_to_utc ]
+        # [ contact_name / platform / comment / location / zone_name / utc_offset ]
         contact_to_change = input('\nEnter contact name/nick to be changed:> ').capitalize()
         if contact_to_change not in InfoBase.select_column('contact_name'):
             print('error: no such contact')
@@ -403,9 +410,9 @@ bbb - go back
                 field_no = input()
                 try:
                     if field_no == 'sss':
-                        print('save')
                         InfoBase.delete_row(contact_to_change)
                         InfoBase.transfer_to_sql(*new_record)
+                        print('saved')
                         break
                     print(record_to_change[0][int(field_no)])
                     new_value = input('Change to:> ')
@@ -427,9 +434,14 @@ bbb - go back
                '33': change_contact,
                }
 
+    @staticmethod
+    def check_if_db_empty():
+        return 0 if not InfoBase.select_all() else 1
+
     def start(self):
         sql_operation = InfoBase()
         sql_operation.create_table()
+        print('Contact base is empty' if self.check_if_db_empty() == 0 else '')
         while True:
             print('\nchoose action:\n00.time operation\n11.add contact\n22.see contact info'
                   '\n33.change contact\n44.exit')
@@ -445,6 +457,14 @@ bbb - go back
 
 
 def main():
+    print('''  _______ _                  ______                  _____                _           _           
+ |__   __(_)                |___  /                 |  __ \              (_)         | |          
+    | |   _ _ __ ___   ___     / / ___  _ __   ___  | |__) |___ _ __ ___  _ _ __   __| | ___ _ __ 
+    | |  | | '_ ` _ \ / _ \   / / / _ \| '_ \ / _ \ |  _  // _ \ '_ ` _ \| | '_ \ / _` |/ _ \ '__|
+    | |  | | | | | | |  __/  / /_| (_) | | | |  __/ | | \ \  __/ | | | | | | | | | (_| |  __/ |   
+    |_|  |_|_| |_| |_|\___| /_____\___/|_| |_|\___| |_|  \_\___|_| |_| |_|_|_| |_|\__,_|\___|_|   
+                                                                                                  
+                                                                                                  ''')
     new = TimeKeeper()
     new.start()
 
